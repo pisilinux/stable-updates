9e588925e69bea89580e920422612f0dc82203ab
vedat <vedat@pisilinux.org>
2014-11-07 01:43:01 +0200

    lsb-release: version bump

---
+++ updates/lsb-release/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+
+def build():
+    #pisitools.dosed("Makefile", "s|n/a|unavailable|", "lsb_release")
+    pisitools.dosed("Makefile", "prefix=.*", "prefix=%s" % get.defaultprefixDIR())
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodir("/etc")
+
+    pisitools.dodoc("README", "ChangeLog")
